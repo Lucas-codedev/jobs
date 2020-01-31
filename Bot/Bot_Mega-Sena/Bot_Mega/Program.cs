478540813a814ce7eaa596880f579a11d893f7dd
Lucas-codedev <60515862+Lucas-codedev@users.noreply.github.com>
2020-01-31 13:01:37 -0300

    A criação de um robô que busca o concurso de dados
    O robô busca dados de um site se usa apis,  usando web crawler.

---
+++ Bot/Bot_Mega-Sena/Bot_Mega/Program.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace Bot_Mega
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informa o numero do concurso:");
+            string NumeroDoConcurso = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(NumeroDoConcurso))
+            {
+                NumeroDoConcurso = "2229";
+            }
+
+            string url = @"http://loterias.caixa.gov.br/wps/portal/loterias/landing/megasena/!ut/p/a1/04_Sj9CPykssy0xPLMnMz0vMAfGjzOLNDH0MPAzcDbwMPI0sDBxNXAOMwrzCjA0sjIEKIoEKnN0dPUzMfQwMDEwsjAw8XZw8XMwtfQ0MPM2I02-AAzgaENIfrh-FqsQ9wNnUwNHfxcnSwBgIDUyhCvA5EawAjxsKckMjDDI9FQE-F4ca/dl5/d5/L2dBISEvZ0FBIS9nQSEh/pw/Z7_HGK818G0KO6H80AU71KG7J0072/res/id=buscaResultado/c=cacheLevelPage/=/?timestampAjax=1580403809220&concurso=" + NumeroDoConcurso;
+            string json;
+
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers["Cookie"] = "security=true";
+                json = wc.DownloadString(url);
+            }
+
+            Resultado resultadoDaMegaSena = JsonConvert.DeserializeObject<Resultado>(json);
+            Console.WriteLine("Concueso selecionado: " + NumeroDoConcurso);
+
+            Console.WriteLine(" ");
+            Console.WriteLine("Resultado");
+            Console.WriteLine("----------------------");
+            Console.WriteLine(resultadoDaMegaSena.resultadoOrdenado);
+            Console.WriteLine("Data do Concurso: " + resultadoDaMegaSena.dataStr);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bot/Bot_Mega-Sena/Bot_Mega/Resultado.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Bot_Mega
+{
+    public class Resultado
+    {
+        public string proximoConcurso { get; set; }
+        public string concursoAnterior { get; set; }
+        public object forward { get; set; }
+        public IList<object> mensagens { get; set; }
+        public long concurso { get; set; }
+        public long data { get; set; }
+        public string resultado { get; set; }
+        public long ganhadores { get; set; }
+        public long ganhadores_quina { get; set; }
+        public long ganhadores_quadra { get; set; }
+        public long valor { get; set; }
+        public double valor_quina { get; set; }
+        public double valor_quadra { get; set; }
+        public long acumulado { get; set; }
+        public double valor_acumulado { get; set; }
+        public long dtinclusao { get; set; }
+        public string prox_final_zero { get; set; }
+        public double ac_final_zero { get; set; }
+        public IList<object> proxConcursoFinal { get; set; }
+        public IList<object> observacao { get; set; }
+        public string rowguid { get; set; }
+        public string ic_conferido { get; set; }
+        public string de_local_sorteio { get; set; }
+        public string no_cidade { get; set; }
+        public string sg_uf { get; set; }
+        public long vr_estimativa { get; set; }
+        public double dt_proximo_concurso { get; set; }
+        public double vr_acumulado_especial { get; set; }
+        public double vr_arrecadado { get; set; }
+        public bool ic_concurso_especial { get; set; }
+        public bool error { get; set; }
+        public bool rateioProcessamento { get; set; }
+        public bool sorteioAcumulado { get; set; }
+        public string concursoEspecial { get; set; }
+        public IList<object> ganhadoresPorUf { get; set; }
+        public string resultadoOrdenado { get; set; }
+        public string dataStr { get; set; }
+        public string dt_proximo_concursoStr { get; set; }
+    }
+}
